59d476e40622d24ee45483bb8fc1877c114c7191
Haleigh <haleighvalentine@gmail.com>
2024-10-16 01:35:31 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'capitalized-comments': ['error', 'always'],
+      'react/prop-types': 'off',
+    },
+  },
+];
